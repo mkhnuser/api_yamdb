1426f8e8bd250222c7a5a0e8f22f1bed8f068afe
Владислав Михеенко <mkhn.email@gmail.com>
2021-05-31 20:46:34 +0300

    CustomProfile model was moved to users app

--- titles/models.py
+++ titles/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    bio = models.TextField(null=True)
-    role = models.CharField(max_length=128)
+# your models
