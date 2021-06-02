68859058065f5451d0b08f0cd8c98067c9a2fb63
Владислав Михеенко <mkhn.email@gmail.com>
2021-06-02 19:44:09 +0300

    Auth app was deleted because of naming problem (django uses that name already)

--- auth/views.py
+++
@@ -1,6 +0,0 @@
-from .serializers import CustomTokenObtainSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer = CustomTokenObtainSerializer
